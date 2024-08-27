44288553d49757a42e3c05e1a202cc7126b2c29b
Tedder Mabanza <teddermabanza@gmail.com>
2024-08-27 07:30:02 +0200

    dash static design completed

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,12 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from articles.models import Article,Category,ArticleImage
 
-# Create your views here.
+
+# Category list view content
+class CategoryListView(ListView):
+    pass
+
+# category delete view content
+def category_delete(request):
+    pass
